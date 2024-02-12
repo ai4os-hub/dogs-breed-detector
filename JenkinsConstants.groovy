7cd9bf41f8916ab7a2f47c32b9005f9c56680d52
Valentin <valentin.kozlov@gmail.com>
2024-02-12 01:36:43 +0100

    testing QA

--- JenkinsConstants.groovy
+++ JenkinsConstants.groovy
@@ -6,8 +6,8 @@ import groovy.transform.Field
 
 //@Field
 //def base_cpu_tag = '"1.12.0-py36"'
-//@Field
-//def base_gpu_tag = '1.12.0-gpu-py36'
+@Field
+def base_gpu_tag = '1.12.0-gpu-py36'
 @Field
 def dockerfile = 'docker/Dockerfile'
 
